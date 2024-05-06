993972bd0fdc5bf928e04ee6b684d12fabce442e
SamyBishay <samybishay3@gmail.com>
2024-05-06 12:25:03 +0200

    signin start td4

---
+++ td4/signin.jsx
@@ -0,0 +1,9 @@
+import {useState} from "react";
+
+function SignIn(props){ 
+    const [Surname, setSurname] = useState("")
+    const [Name, setName] = useState("")
+    const [Login, setLogin] = useState("")
+    const [Pass, setPass] = useState("")
+
+}
